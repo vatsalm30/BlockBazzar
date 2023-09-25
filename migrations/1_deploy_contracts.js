bda116a19b3819cb4cf6c3af09d5d914d03194fa
vatsalm30 <vatsalm30@gmail.com>
2023-09-25 09:20:39 -0400

    c

--- migrations/1_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-var Market = artifacts.require("./Market.sol");
-var SaleTokens = artifacts.require("./SaleTokens.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(Market);
-  deployer.deploy(SaleTokens);
-};
